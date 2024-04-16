58d5ce728428e70027913f7668b42304d0968d24
Sunny More <mxsunny10@gmail.com>
2024-04-16 21:49:52 +0530

    Create EmpManSystemApplication.java

---
+++ emp-man-system/src/main/java/com/sunny/empmansystem/controller/EmpManSystemApplication.java
@@ -0,0 +1,13 @@
+package com.sunny.empmansystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpManSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmpManSystemApplication.class, args);
+	}
+
+}
